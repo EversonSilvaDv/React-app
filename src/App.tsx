c561121a58ef93b0b44e2157db1667305f9621a2
EversonSilvaDv <brothers1914@gmail.com>
2022-10-10 15:48:55 -0300

    correção link audio

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>App-teste-audio</h1>
-      <audio src='public/vivaldi.mp3' controls></audio>
+      <audio src='./vivaldi.mp3' controls></audio>
     </div>
   );
 }
